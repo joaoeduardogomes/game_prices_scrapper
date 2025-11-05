2dbc8a6189fd5152d49d337946b5e32b37ec84e8
João Gomes <joaoeduardo.gomes21@gmail.com>
2025-11-05 19:11:49 -0300

    update db and index

--- db.js
+++ db.js
@@ -2,26 +2,31 @@ const { MongoClient } = require('mongodb')
 require('dotenv').config()
 
 const password = process.env.PASSWORD
+const DB_NAME = "projects"
+const COLLECTION_NAME = "game-prices-scrapper"
 
 
 async function run() {
     const uri = `mongodb+srv://joaoGomes:${password}@t3ste.ckcoclb.mongodb.net/`
     const client = new MongoClient(uri)
-    
+
     try {
 
-        const database = client.db("projects")
-        const gamesDb = database.collection("game-prices-scrapper")
+        const database = client.db(DB_NAME)
+        const gamesDb = database.collection(COLLECTION_NAME)
 
-        const projectionOptions = { 
+        const projectionOptions = {
             projection: {
-                _id: 0    // Exclua o _id
+                _id: 0
             }
         };
         const allGames = await gamesDb.find({}, projectionOptions).toArray()
 
         console.log(allGames)
 
+    } catch (error) {
+        console.error("Error trying to access the database:", error);
+        throw error;
     } finally {
         await client.close();
     }
--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.sendFile("index.html", {root: "public"})
 })
+app.get('/api/games', (req, res) => {
+    
+})
 
 app.listen(port, () => {
     console.log(`App running on http://localhost:${port}/`)

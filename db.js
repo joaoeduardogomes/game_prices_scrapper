56600107d27ec079bd6f3ed43b3d9e06a0ca8d1b
João Gomes <joaoeduardo.gomes21@gmail.com>
2025-11-10 18:34:48 -0300

    updated db.js
    Replaced the test code for an usual one

--- db.js
+++ db.js
@@ -4,33 +4,53 @@ require('dotenv').config()
 const password = process.env.PASSWORD
 const DB_NAME = "projects"
 const COLLECTION_NAME = "game-prices-scrapper"
+let client
+let db
 
-
-async function run() {
+async function connectToDatabase() {
     const uri = `mongodb+srv://joaoGomes:${password}@t3ste.ckcoclb.mongodb.net/`
-    const client = new MongoClient(uri)
+    
+    if (db) return db
+    
+    try {
+
+        client = new MongoClient(uri)
+        await client.connect()
+        
+        db = client.db(DB_NAME)
+        return db
+    }
+    catch(error) {
+        if (client)
+            await client.close()
+
+        throw error
+    }
+}
+
+async function getAllGames() {
+    if (!db)
+        throw new Error("Connection not established")
 
     try {
 
-        const database = client.db(DB_NAME)
-        const gamesDb = database.collection(COLLECTION_NAME)
-
+        const gamesDb = db.collection(COLLECTION_NAME)
         const projectionOptions = {
             projection: {
                 _id: 0
             }
-        };
+        }
         const allGames = await gamesDb.find({}, projectionOptions).toArray()
-
-        console.log(allGames)
-
-    } catch (error) {
-        console.error("Error trying to access the database:", error);
-        throw error;
-    } finally {
-        await client.close();
+        
+        return allGames
     }
-
+    catch(error) {
+        console.error("Error trying to access the database:", error)
+        throw error
+    }
 }
 
-run().catch(console.dir)
+module.exports = {
+    connectToDatabase,
+    getAllGames
+}

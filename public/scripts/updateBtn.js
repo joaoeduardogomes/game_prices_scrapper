9bfc9452c78ae0671b33cfeb5c653bb3102e5a20
João Gomes <joaoeduardo.gomes21@gmail.com>
2025-12-03 20:46:58 -0300

    remove btn + add favicon

---
+++ public/scripts/updateBtn.js
@@ -0,0 +1,16 @@
+const updateBtn = document.querySelector("button#update-btn")
+const warningSpan = document.querySelector("span#warning")
+
+updateBtn.addEventListener("click", async () => {
+    warningSpan.textContent = "Please wait the scraping to complete. The data will update automatically."
+
+    try {
+        const res = await fetch("/api/games/update", { method: "POST" })
+        if (!res.ok) throw new Error("Failed to update")
+    } catch (err) {
+        warningSpan.textContent = "Something went wrong!"
+        return
+    }
+
+    location.reload()
+})
